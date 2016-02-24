862bfaa71aba9df12b708e3d67ff077d23799753
greg <gregpechiro@gmail.com>
2016-02-24 16:49:03 -0500

    updated settings

--- dndtools/wsgi.py
+++ dndtools/wsgi.py
@@ -1,12 +1,7 @@
 import os
 import sys
 
-#sys.path.append('/opt/dndtools')
-#os.environ["DJANGO_SETTINGS_MODULE"] = "dndtools.settings"
-#activate_this = '/opt/dndtools/env/bin/acivatate_this.py'
-#execfile(activate_this, dict(__file__=activate_this))
-
 sys.path = ['/opt/dndtools/' , '/opt/dndtools/dndtools/'] + sys.path
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dndtools.settings")
-activate_this = '/opt/dndtools/env/bin/activate_this.py'
+import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
